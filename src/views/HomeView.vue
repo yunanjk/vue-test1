2575b74357b2dce3070855f384105a07c8490f0d
oyy <oyy0821@chunjae.co.kr>
2023-11-21 13:24:47 +0900

    인스턴스 라이프 사이클

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
     <h1>This is home page</h1>
-    <form action="">
-      <!-- <label for="">Name</label>
-      <input type="text" v-model="name" style="margin: 10px; padding: 20px; border: 3px solid green;">-->
-      <InputField :name="name" @update-name="updateName"/>
-      <button>submit</button>
-    </form>
+    <InputField :name="name"/>
+    <button @click="updateName">submit</button>
     {{ name }}
   </div>
 </template>
@@ -16,18 +12,48 @@ import InputField from "@/components/InputField.vue";
 
 export default {
   name: 'HomeView',
-  data() {
-    return {
-      name: ""
-    }
-  },
-  methods: {
-    updateName(name) {
-      this.name = name;
-    }
-  },
   components: {
     InputField
+  },
+  data() {
+    return {
+      name: "Kossie Coder"
+    }
+  },
+
+  // 인스턴스 라이프사이클
+  beforeCreate() {
+    console.log("beforeCreate", this.name);
+  },
+  created() {
+    console.log("created", this.name);
+  },
+
+  beforeMount() {
+    alert("beforeMount");
+  },
+  mounted() {
+    alert("mounted");
+  },
+
+  beforeUpdate() {
+    alert("beforeUpdate");
+  },
+  updated() {
+    alert("updated");
+  },
+
+  beforeUnmount() {
+    alert("beforeUnmount");
+  },
+  unmounted() {
+    alert("unmounted");
+  },
+  
+  methods: {
+    updateName() {
+      this.name = "hello";
+    }
   }
 }
 </script>
